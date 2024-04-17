816a47ffdc0a49285c662ab21414a15b094ceab1
illyalane <lane_illya@yahoo.com>
2024-04-17 19:56:31 -0400

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,5 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def learn(self, subject):
+        self.knowledge.append(subject)
--- lib/teacher.py
+++ lib/teacher.py
@@ -7,4 +7,7 @@ import random
 class Teacher(User):
 
     def teach(self):
-        pass
+        
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        random_knowledge = self.knowledge[random_index]
+        return random_knowledge   
